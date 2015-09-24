67ce36065894a524c7b263cbd4359d482976c8d3
Seiti Yamashiro <seiti@loggi.com>
2015-09-24 20:11:00 -0300

    setting more sane defaults

--- main.go
+++ main.go
@@ -31,9 +31,9 @@ func main() {
 	// flags. love flags.
 	region := flag.String("region", "sa-east-1", "AWS region code.") // default is 'são paulo' region
 	retries := flag.Int("retries", 10, "Number of retries")
-	chunkSize := flag.Int64("chunk-size", int64(100), "Size of each portion fetched")
-	fetchNap := flag.Duration("fetch-nap", 5 * time.Minute, "Nap time between fetches. Each fetch usually creates a single file")
-	portionNap := flag.Duration("portion-nap", 5 * time.Second, "Nap time between each portion fetch")
+	chunkSize := flag.Int64("chunk-size", int64(10000), "Size of each portion fetched")
+	fetchNap := flag.Duration("fetch-nap", 1 * time.Minute, "Nap time between fetches. Each fetch usually creates a single file")
+	portionNap := flag.Duration("portion-nap", 10 * time.Second, "Nap time between each portion fetch")
 	logLevel := flag.String("log-level", "info", "Log level of this app, not RDS PG, as defined by logrus package")
 	instanceId := flag.String("instance-id", "theInstanceId", "RDS instance id") // this one is mandatory, shouldn't be really a flag. oh well...
 	retrievedFileDir := flag.String("retrieved-file-dir", "/tmp", "Dir where to put downloaded files")
@@ -49,8 +49,6 @@ func main() {
 	check(error, "Level not understood", log.Fields{"logLevel": logLevel})
 	log.SetLevel(level)
 
-	log.Infoln("All set. Starting.")
-
 	svc := rds.New(aws.NewConfig().WithRegion(*region).WithMaxRetries(*retries))
 	p = Param {
 		svc: svc,
@@ -61,6 +59,16 @@ func main() {
 		retrievedFileDir: retrievedFileDir,
 		runAsService: runAsService,
 	}
+
+	log.WithFields(log.Fields{
+		"instanceId": *p.instanceId,
+		"fetchNap": *p.fetchNap,
+		"chunkSize": *p.chunkSize,
+		"portionNap": *p.portionNap,
+		"runAsService": *p.runAsService,
+		"retrievedFileDir": *p.retrievedFileDir,
+		"logLevel": level,
+	}).Info("All set. Starting.")
 
 	// Steps
 	//
@@ -77,6 +85,10 @@ func main() {
 
 		// transition time!
 		if pglog != nil && currPglog != pglog {
+			log.WithFields(log.Fields{
+				"pglog": pglog,
+				"currPgLog": currPglog,
+			}).Debug("Transition time!")
 			// retrieve remainder of currPgLog
 			fetchData(p, pglog, &marker)
 			pglog = currPglog
@@ -85,6 +97,7 @@ func main() {
 
 		currMarker = fetchData(p, currPglog, &currMarker)
 		firstLoop = false
+		log.Debugln("Fetching nap ZZZzzzz...")
 		time.Sleep(*p.fetchNap)
 	}
 	log.Infoln("Done. Should not get here, really.")
@@ -100,7 +113,7 @@ func fetchData(p Param, pglog *string, marker *string) string {
 	_, err := f.WriteString(*portion.LogFileData)
 	check(err, "Couldn't write to file", log.Fields{"filename" :filename})
 	for *portion.AdditionalDataPending {
-		log.Debugln("ZZZzzzz...")
+		log.Debugln("Portion nap ZZZzzzz...")
 		time.Sleep(*p.portionNap)
 		currMarker = portion.Marker
 		portion = downloadLogFilePortion(p, pglog, *currMarker)
@@ -148,11 +161,11 @@ func downloadLogFilePortion(p Param, pglog *string, marker string) (*rds.Downloa
 		Marker:               aws.String(marker),
 		NumberOfLines:        p.chunkSize,
 	}
-	log.WithFields(log.Fields{"req_marker_add": params.Marker, "req_marker_value": *params.Marker}).Debug()
+	log.WithFields(log.Fields{"marker_add": params.Marker, "marker_value": *params.Marker}).Debug("Request")
 	resp, err := p.svc.DownloadDBLogFilePortion(params)
 	check(err, "Couldn't get a portion of file", log.Fields{ "logFilename":  *params.LogFileName})
-	log.WithFields(log.Fields{"resp_marker_add": resp.Marker, "resp_marker_value": *resp.Marker}).Debug()
-	log.WithFields(log.Fields{"additional_data_pending": *resp.AdditionalDataPending}).Debug()
+	log.WithFields(log.Fields{"marker_add": resp.Marker, "marker_value": *resp.Marker}).Debug("Response")
+	log.WithFields(log.Fields{"additional_data_pending": *resp.AdditionalDataPending}).Debug("Data pending?")
 	log.WithFields(log.Fields{"data_len": len(*resp.LogFileData)}).Debug("Fetched chars")
 	return resp
 }
