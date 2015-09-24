c75475e4f79998f9b1936c533bd4ae3d05248981
Seiti Yamashiro <seiti@loggi.com>
2015-09-24 16:04:09 -0300

    code overhaul. accepting args. downloaded file ok

--- main.go
+++ main.go
@@ -1,59 +1,139 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"time"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/rds"
+	"strings"
 )
 
+// Dowloads
 func main() {
 
-	// some consts
-	// TODO seiti - extract to args
-	region := "sa-east-1"
-	instanceId := "staging-db-rds"
-	retries := 10
-	maxRecords := int64(2)
+	// flags. love flags.
+	region := flag.String("region", "sa-east-1", "AWS region code.") // default is 'são paulo' region
+	retries := flag.Int("retries", 10, "Number of retries")
+	chunkSize := flag.Int64("chunk-size", int64(100), "Size of each portion fetched")
+	napDuration := flag.Duration("nap-duration", 500 * time.Millisecond, "Nap time between portion fetch")
+	logLevel := flag.String("log-level", "info", "Log level of this app, not RDS PG, as defined by logrus package")
+	instanceId := flag.String("instance-id", "theInstanceId", "RDS instance id") // this one is mandatory, shouldn't be really a flag. oh well...
+	retrievedFileDir := flag.String("retrieved-file-dir", "/tmp", "Dir where to put downloaded files")
+	flag.Parse()
 
-	svc := rds.New(aws.NewConfig().WithRegion(region).WithMaxRetries(retries))
-
-	resp, err := listDBLogFiles(svc, instanceId, maxRecords)
-	if err != nil {
-		errProcessing(err)
+	if len(os.Args) <= 1 {
+		flag.PrintDefaults()
+		return
 	}
 
-	// TODO seiti - remove this
-	fmt.Println(resp.DescribeDBLogFiles)
+	level, error := log.ParseLevel(*logLevel)
+	check(error, "Level not understood", log.Fields{"logLevel": logLevel})
+	log.SetLevel(level)
+
+	log.Infoln("All set. Starting.")
+
+	// Steps
+	//
+	// Looping while true (has to run as a service after daemonizing it)
+	//     Loop until discover file to download (check recover situations)
+	//     Loop while the file has content not downloaded AND there is no new file to download
+	//         download, append and save file content
+	svc := rds.New(aws.NewConfig().WithRegion(*region).WithMaxRetries(*retries))
+
+	logFilename := logFileDiscover(svc, instanceId)
+	portion, _ := downloadLogFilePortion(svc, instanceId, logFilename, "0", *chunkSize)
+
+	filename := *retrievedFileDir + "/" + *logFilename
+	splitedPath := strings.Split(filename, "/")
+	dir := strings.Join(splitedPath[:len(splitedPath) -1], "/")
+	os.MkdirAll(dir, 0700)
+	log.WithFields(log.Fields{
+		"filename": filename,
+		"splitedPath": splitedPath,
+		"dir": dir,
+	}).Info()
+
+	f, err := os.Create(filename)
+	check(err, "Error creating", log.Fields{"filename":  filename})
+	defer f.Close()
+
+	_, err = f.WriteString(*portion.LogFileData)
+	check(err, "Couldn't write to file", log.Fields{"filename" :filename})
+
+	// check current and expected number of lines
+	for *portion.AdditionalDataPending {
+		time.Sleep(*napDuration)
+		portion, _ = downloadLogFilePortion(svc, instanceId, logFilename, *portion.Marker, *chunkSize)
+		_, err = f.WriteString(*portion.LogFileData)
+		check(err, "Couldn't write to output file T_T", log.Fields{})
+		f.Sync()
+	}
+
+	log.Infoln("Done. Should not get here, really.")
 }
 
-func listDBLogFiles(svc *rds.RDS, instanceId string, maxRecords int64) (*rds.DescribeDBLogFilesOutput, error) {
+func logFileDiscover(svc *rds.RDS, instanceId *string) *string {
+	// TODO XXX seiti - remove line below. this one is a good log, as it is very small (241 lines)
+	test := aws.String("error/postgresql.log.2015-09-24-10")
+	log.Debugln(test)
+
+	return test
+	// TODO seiti - uncomment
+	//	resp, err := listLogFiles(svc, instanceId)
+	// check(err, "")
+	//	last := resp.DescribeDBLogFiles[len(resp.DescribeDBLogFiles) -1]
+	//	log.Debugln(last)
+	//	return last
+}
+
+
+func listLogFiles(svc *rds.RDS, instanceId string) (*rds.DescribeDBLogFilesOutput, error) {
 	params := &rds.DescribeDBLogFilesInput{
 		DBInstanceIdentifier: aws.String(instanceId),
-		MaxRecords:           aws.Int64(maxRecords),
 	}
-	return svc.DescribeDBLogFiles(params)
+	resp, err := svc.DescribeDBLogFiles(params)
+	check(err, "Couldn't get the logs list", log.Fields{ "instanceId": instanceId})
+	log.Debugln(resp.DescribeDBLogFiles)
+	return resp, err
 }
 
-func downloadDBLogFilePortion(svc *rds.RDS, instanceId string, logFilename string, marker string, nlines int64) (*rds.DownloadDBLogFilePortionOutput, error) {
+func downloadLogFilePortion(svc *rds.RDS, instanceId *string, logFilename *string, marker string, nlines int64) (*rds.DownloadDBLogFilePortionOutput, error) {
 	params := &rds.DownloadDBLogFilePortionInput{
-		DBInstanceIdentifier: aws.String(instanceId),
-		LogFileName:          aws.String(logFilename),
+		DBInstanceIdentifier: instanceId,
+		LogFileName:          logFilename,
 		Marker:               aws.String(marker),
 		NumberOfLines:        aws.Int64(nlines),
 	}
-	return svc.DownloadDBLogFilePortion(params)
+
+	log.Debugln(fmt.Sprintf("req marker %v: %v", params.Marker, *params.Marker))
+	resp, err := svc.DownloadDBLogFilePortion(params)
+	check(err, "Couldn't get a portion of file", log.Fields{ "logFilename":  *params.LogFileName})
+	log.Debugln(fmt.Sprintf("resp marker %v: %v", resp.Marker, *resp.Marker))
+
+	log.Debugln(*resp.LogFileData)
+	log.Debugln(fmt.Sprintf("is additional data pending: '%v'", *resp.AdditionalDataPending))
+	return resp, err
 }
 
-func errProcessing(err error) {
+func check(err error, panicMsg string, panicFields log.Fields) {
+	if err == nil {
+		return
+	}
+	log.WithError(err)
 	if awsErr, ok := err.(awserr.Error); ok {
-		log.Errorln(awsErr.Code(), awsErr.Message())
-		log.WithError(err)
+		log.WithFields(log.Fields{
+			"awserr_code": awsErr.Code(),
+			"awserr_msg": awsErr.Message(),
+		}).Error()
 		if origErr := awsErr.OrigErr(); origErr != nil {
 			log.WithError(origErr)
 		}
-	} else {
-		log.WithError(err)
 	}
+	log.WithFields(panicFields).Panic(panicMsg)
+	panic(panicMsg)
 }
